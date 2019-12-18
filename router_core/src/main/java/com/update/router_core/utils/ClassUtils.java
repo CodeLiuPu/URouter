8c2cff2af536c45c8250492588a8478cf5cbc545
liupu <575866447@qq.com>
2019-12-18 14:57:52 +0800

    ClassUtils

--- router_core/src/main/java/com/update/router_core/utils/ClassUtils.java
+++ router_core/src/main/java/com/update/router_core/utils/ClassUtils.java
@@ -6,12 +6,19 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 
+import com.update.router_core.thread.DefaultPoolExecutor;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import dalvik.system.DexFile;
 
 /**
  * @author : liupu
@@ -23,20 +30,51 @@ public class ClassUtils {
 
     private ClassUtils(){}
 
-
-
     /**
      * 路由表
      */
-    public static Set<String> getFileNameByPackageName(Application context, String packageName) throws PackageManager.NameNotFoundException {
-        Set<String> classNames = new HashSet<>();
+    public static Set<String> getFileNameByPackageName(Application context, final String packageName)
+            throws PackageManager.NameNotFoundException, InterruptedException {
+        final Set<String> classNames = new HashSet<>();
         List<String> paths = getSourcePaths(context);
 
         // 使用同步计数器判断 处理完成
-        CountDownLatch parserCtl = new CountDownLatch(paths.size());
+        final CountDownLatch parserCtl = new CountDownLatch(paths.size());
+        ThreadPoolExecutor threadPoolExecutor = DefaultPoolExecutor.newDefaultPoolExecutor(paths.size());
 
-
-
+        for (final String path: paths){
+            threadPoolExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    DexFile dexFile = null;
+                    try {
+                        // 加载 apk 中的 dex, 并遍历 获得所有包名为 {packageName} 的类
+                        dexFile = new DexFile(path);
+                        Enumeration<String> dexEntries = dexFile.entries();
+                        while (dexEntries.hasMoreElements()){
+                            String  className = dexEntries.nextElement();
+                            if (className.startsWith(packageName)){
+                                classNames.add(className);
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (null != dexFile){
+                            try {
+                                dexFile.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        // 释放1个
+                        parserCtl.countDown();
+                    }
+                }
+            });
+        }
+        // 等待执行完成
+        parserCtl.await();
         return classNames;
     }
 
