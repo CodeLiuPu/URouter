3f98c4ae04ed8f30559f3799229a69b2cf5f7b44
liupu <575866447@qq.com>
2019-12-18 17:11:30 +0800

    add base

---
+++ base/src/main/java/com/update/base/TestService.java
@@ -0,0 +1,13 @@
+package com.update.base;
+
+import com.update.router_core.template.IService;
+
+/**
+ * @author : liupu
+ * date   : 2019/12/18
+ * desc   :
+ * github : https://github.com/CodeLiuPu/
+ */
+public interface TestService extends IService {
+    void test();
+}
