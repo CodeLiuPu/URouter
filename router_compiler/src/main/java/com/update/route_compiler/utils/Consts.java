64b7729ebe61f07a580c152d12cf31b516b1611f
liupu <575866447@qq.com>
2019-12-18 12:20:26 +0800

    Consts

--- router_compiler/src/main/java/com/update/route_compiler/utils/Consts.java
+++ router_compiler/src/main/java/com/update/route_compiler/utils/Consts.java
@@ -1,5 +1,7 @@
 package com.update.route_compiler.utils;
 
+import com.squareup.javapoet.ClassName;
+
 /**
  * @author : liupu
  * date   : 2019/12/18
@@ -7,4 +9,51 @@ package com.update.route_compiler.utils;
  * github : https://github.com/CodeLiuPu/
  */
 public class Consts {
+
+    public static final ClassName router =
+            ClassName.get("com.dn_alan.router_core", "DNRouter");
+
+    public static final String IROUTE_GROUP = "";
+
+    public static final String ACTIVITY = "android.app.Activity";
+    public static final String ISERVICE = "com.update.router_core";
+
+
+
+
+    private static final String LANG = "java.lang.";
+    public static final String BYTE = LANG + "Byte";
+    public static final String SHORT = LANG + "Short";
+    public static final String INTEGER = LANG + "Integer";
+    public static final String LONG = LANG + "Long";
+    public static final String FLOAT = LANG + "Float";
+    public static final String DOUBLE = LANG + "Double";
+    public static final String BOOLEAN = LANG + "Boolean";
+    public static final String STRING = LANG + "String";
+
+    public static final String ARRAY = "ARRAY";
+
+    public static final String BYTEARRAY = "byte[]";
+    public static final String SHORTARRAY = "short[]";
+    public static final String INTARRAY = "int[]";
+    public static final String LONGARRAY = "long[]";
+    public static final String BOOLEANARRAY = "boolean[]";
+    public static final String CHARARRAY = "char[]";
+    public static final String FLOATARRAY = "float[]";
+    public static final String DOUBLEARRAY = "double[]";
+    public static final String STRINGARRAY = STRING + "[]";
+
+    public static final String ARRAYLIST = "java.util.ArrayList";
+    public static final String LIST = "java.util.List";
+
+    public static final String PARCELABLE = "android.os.Parcelable";
+
+    public static final String SEPARATOR = "$$";
+    public static final String PROJECT = "URouter";
+
+    public static final String NAME_OF_ROOT = PROJECT + SEPARATOR + "Root" + SEPARATOR;
+    public static final String NAME_OF_GROUP = PROJECT + SEPARATOR + "Group" + SEPARATOR;
+    public static final String NAME_OF_EXTRA = SEPARATOR + "Extra";
+
+    public static final String PACKAGE_OF_GENERATE_FILE = "com.update.router_core";
 }
