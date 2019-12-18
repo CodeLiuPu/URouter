186ee5a19b55136c812df764cbf9140ff5e95be4
liupu <575866447@qq.com>
2019-12-18 17:15:48 +0800

    add ModuleActivity

---
+++ module1/src/main/java/com/update/module1/Module1Activity.java
@@ -0,0 +1,7 @@
+package com.update.module1;
+
+import android.app.Activity;
+
+public class Module1Activity extends Activity {
+
+}
---
+++ module2/src/main/java/com/update/module2/Module2Activity.java
@@ -0,0 +1,7 @@
+package com.update.module2;
+
+import android.app.Activity;
+
+public class Module2Activity extends Activity {
+
+}
