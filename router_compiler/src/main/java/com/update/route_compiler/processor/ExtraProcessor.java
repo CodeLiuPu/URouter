58532e643df8beb5d7a4d36b6a5833e153156b05
liupu <575866447@qq.com>
2019-12-19 15:24:05 +0800

    ExtraProcessor # categories()

--- router_compiler/src/main/java/com/update/route_compiler/processor/ExtraProcessor.java
+++ router_compiler/src/main/java/com/update/route_compiler/processor/ExtraProcessor.java
@@ -1,10 +1,98 @@
 package com.update.route_compiler.processor;
 
+import com.google.auto.service.AutoService;
+import com.update.route_compiler.utils.Consts;
+import com.update.route_compiler.utils.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedOptions;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+
 /**
  * @author : liupu
  * date   : 2019/12/18
  * desc   :
  * github : https://github.com/CodeLiuPu/
  */
-public class ExtraProcessor {
+@AutoService(Processor.class)
+@SupportedOptions(Consts.ARGUMENTS_NAME)
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
+@SupportedAnnotationTypes(Consts.ANN_TYPE_EXTRA)
+public class ExtraProcessor extends AbstractProcessor {
+
+    /**
+     * 节点工具类 (类、函数、属性都是节点)
+     */
+    private Elements elementUtils;
+
+    /**
+     * type(类信息)工具类
+     */
+    private Types typeUtils;
+    /**
+     * 类/资源生成器
+     */
+    private Filer filerUtils;
+
+    /**
+     * 记录所有需要注入的属性 key:类节点 value:需要注入的属性节点集合
+     */
+    private Map<TypeElement, List<Element>> parentAndChild = new HashMap<>();
+    private Log log;
+
+    /**
+     * 初始化 从 {@link ProcessingEnvironment} 中获得一系列处理器工具
+     */
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnvironment) {
+        super.init(processingEnvironment);
+        //获得apt的日志输出
+        log = Log.newLog(processingEnvironment.getMessager());
+        elementUtils = processingEnv.getElementUtils();
+        typeUtils = processingEnvironment.getTypeUtils();
+        filerUtils = processingEnv.getFiler();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        return false;
+    }
+
+    private void generateAutoWired() throws IOException {
+
+    }
+
+    /**
+     * 记录需要生成的类和属性
+     */
+    private void categories(Set<? extends Element> elements) throws IllegalAccessException {
+        for (Element element : elements) {
+            // 获得父节点 (类)
+            TypeElement enclosingElement = (TypeElement) element.getEnclosingElement();
+            if (parentAndChild.containsKey(enclosingElement)) {
+                parentAndChild.get(enclosingElement).add(element);
+            } else {
+                List<Element> childs = new ArrayList<>();
+                childs.add(element);
+                parentAndChild.put(enclosingElement, childs);
+            }
+        }
+    }
 }
